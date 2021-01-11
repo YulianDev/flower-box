7c8b9a0383ec4cbd60cb4538dd325d3544cfa45a
Yulian <uliarx@gmail.com>
2021-01-12 00:29:18 +0200

    product page half part

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
 $(function(){
+
+    $('.product-card__input').styler();
+
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        draggable: false,
+        arrows: false,
+
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        arrows:false, 
+        fade: true,
+    });
+
     $('.star').rateYo({
         starWidth: '16px',
         fullStar: true,
@@ -6,17 +24,18 @@ $(function(){
         spacing: '5px',
     });
 
-    const menu = $('.menu');
-    const menuHeight = $(menu).height();
-    const menuOffset = menu.offset().top;
+    const menuMarginTop = $('.menu').offset().top;
+    const menuHeight = $('.menu').height();
     $(window).on('scroll', function(){
         const scrolled = $(this).scrollTop();
-        if(scrolled > menuOffset) {
-            $('.header').css({marginBottom: menuHeight});
-        } else if (scrolled < menuOffset) {
-            $('.header').css({marginBottom: 0});
+        if(scrolled > menuMarginTop) {
+            $('.header').addClass('header-fixed');
+            $('.header__inner').css({marginBottom:menuHeight});
+        } else if(scrolled < menuMarginTop) {
+            $('.header').removeClass('header-fixed');
+            $('.header__inner').css({marginBottom:0});
         }
-    })
+    });
 
     /* let mixer = mixitup('.product__items') */
 });
