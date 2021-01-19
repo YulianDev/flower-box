ff7a84b530a1661ceb9002328320d38f443af192
Yulian <uliarx@gmail.com>
2021-01-19 02:10:57 +0200

    created filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
+    $('.filter-price__input').ionRangeSlider({
+        type: 'double',
+        onStart: function (data) {
+            $('.filter-price__from').text(data.from) ;
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
     /* $('.reply-form__text-area').on('scroll', function() {
         let currentHeight = 0;
         if($(this).scrollTop() > 0) {
